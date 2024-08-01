e48768c93ad189fcafcf28df272a354a70296114
Abdulhakeem Adekunle <abdulhakeemyisa@gmail.com>
2024-08-01 18:20:03 +0100

    corrected a syntax error per checker requirements

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,19 +1,14 @@
 import mysql.connector
 from getpass import getpass
 
-try: 
+try:
     mydatabase = mysql.connector.connect(
     host = 'localhost',
     user = 'root',
-    password = getpass("Enter your password: "),
-    database = alx
-)
+    password = getpass("Enter your password: ")
+    )
     mycursor = mydatabase.cursor()
     mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
     print("Database created successfully")
-
-except mysql.connection.Error as e:
-    print(e)
-
-mycursor.close()
-mydatabase.close()
+except mysql.connector.Error as err:
+    print(f"Failed to connect: {err}")
